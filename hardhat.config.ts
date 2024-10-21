4b0a917dd92aab0a353171a3e280d0f1d158c545
Kerem Gurel <keremgurell99@gmail.com>
2024-10-21 00:05:25 -0700

    add basescan api key

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,6 +2,7 @@ import { HardhatUserConfig, vars } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const ETHERSCAN_API_KEY = vars.get("ETHERSCAN_API_KEY");
+const BASESCAN_API_KEY = vars.get("BASESCAN_API_KEY");
 const INFURA_API_KEY = vars.get("INFURA_API_KEY");
 const ALCHEMY_API_KEY = vars.get("ALCHEMY_API_KEY");
 const SEPOLIA_PRIVATE_KEY = vars.get("SEPOLIA_PRIVATE_KEY");
@@ -21,10 +22,11 @@ const config: HardhatUserConfig = {
   etherscan: {
     apiKey: {
       sepolia: ETHERSCAN_API_KEY,
+      base_sepolia: BASESCAN_API_KEY,
     },
     customChains: [
       {
-        network: "baseSepolia",
+        network: "base_sepolia",
         chainId: 84532,
         urls: {
           apiURL: "https://api-sepolia.basescan.org/api",
